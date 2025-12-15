94a90da91759e9c039b8f85454b6a2e40f6da4aa
Batch1 <Batch1@VITUNIVERSITY.LOCAL>
2025-12-15 12:11:12 +0530

    first commit

---
+++ test.java
@@ -0,0 +1,25 @@
+class test {
+    public static void main(String[] args) {
+        int rows = 3, cols = 3;
+        int[][] matrix1 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[][] matrix2 = {{9, 8, 7}, {6, 5, 4}, {3, 2, 1}};
+        int[][] sumMatrix = new int[rows][cols];
+
+        System.out.println("Adding two matrices...");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                sumMatrix[i][j] = matrix1[i][j] + matrix2[i][j];
+            }
+        }
+
+        System.out.println("Resultant Matrix:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(sumMatrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\nRegistration Number: 23BBS0123");
+    }
+}
